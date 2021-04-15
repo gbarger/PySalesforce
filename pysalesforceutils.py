33b874630f6cb622391f13d70b5ca85a349727ad
Crystal <>
2021-04-15 10:53:26 -0700

    add get_sobject_rows()

--- pysalesforceutils.py
+++ pysalesforceutils.py
@@ -857,6 +857,41 @@ class Standard:
         return response_text
 
     @staticmethod
+    def get_sobject_rows(object_name, record_id_list_string, field_list_string, access_token, instance_url):
+        """
+        Provides the details requested for the specified record id list. In practice, if you
+        provide an explicit list of fields, it will be just like a query for that
+        record, but if you leave the fields blank, this will return a lot if not
+        all fields. I'm not sure about that because the description of what is
+        returned if you leave the fields empty isn't explained in the API documentation
+
+        Args:
+            object_name (str): The API name of the object.
+            record_id_list_string (str): The record id list you're trying to retrieve
+            field_list_string (str): List of comma separated values for fields
+                                     to retrieve
+            access_token (str): This is the access_token value received from the
+                                login response
+            instance_url (str): This is the instance_url value received from the
+                                login response
+
+        Returns:
+            dict: returns the record with the explicit field list, or all (or
+                a lot) of the fields if the field_list_string is None.
+        """
+
+        get_row_uri = '/composite/sobjects/' + object_name
+        header_details = Util.get_standard_header(access_token)
+
+        get_rows_uri = get_row_uri + '?ids=' + record_id_list_string + '&fields=' + field_list_string
+
+        response = webservice.Tools.get_http_response(
+            instance_url + Standard.base_standard_uri + 'v' + API_VERSION + get_rows_uri, header_details)
+        json_response = json.loads(response.text)
+
+        return json_response
+
+    @staticmethod
     def create_sobject_rows(records, all_or_none, run_assignment_rules, access_token, instance_url):
         """
         Creates a list of up to 200 records.
@@ -1375,37 +1410,37 @@ class Standard:
     @staticmethod
     def get_updated(object_name, start_date_time, end_date_time, access_token, instance_url):
         """
-        Retrieves the list of individual records that have been updated (added 
-        or changed) within the given timespan for the specified object. SObject 
+        Retrieves the list of individual records that have been updated (added
+        or changed) within the given timespan for the specified object. SObject
         Get Updated is available in API version 29.0 and later.
 
-        This resource is commonly used in data replication applications. Note 
+        This resource is commonly used in data replication applications. Note
         the following considerations:
-            * Results are returned for no more than 30 days previous to the day 
+            * Results are returned for no more than 30 days previous to the day
               the call is executed.
-            * Your client application can replicate any objects to which it has 
-              sufficient permissions. For example, to replicate all data for 
-              your organization, your client application must be logged in with 
-              “View All Data” access rights to the specified object. Similarly, 
+            * Your client application can replicate any objects to which it has
+              sufficient permissions. For example, to replicate all data for
+              your organization, your client application must be logged in with
+              “View All Data” access rights to the specified object. Similarly,
               the objects must be within your sharing rules.
-            * There is a limit of 600,000 IDs returned from this resource. If 
-              more than 600,000 IDs would be returned, EXCEEDED_ID_LIMIT is 
-              returned. You can correct the error by choosing start and end 
+            * There is a limit of 600,000 IDs returned from this resource. If
+              more than 600,000 IDs would be returned, EXCEEDED_ID_LIMIT is
+              returned. You can correct the error by choosing start and end
               dates that are closer together.
 
         Args:
             object_name (str): The name of the object to get updated records for.
 
-            startDateTime (datetime): Starting date/time (Coordinated Universal 
-                Time (UTC) time zone—not local— timezone) of the timespan for 
-                which to retrieve the data. The API ignores the seconds portion 
-                of the specified dateTime value (for example, 12:30:15 is 
+            startDateTime (datetime): Starting date/time (Coordinated Universal
+                Time (UTC) time zone—not local— timezone) of the timespan for
+                which to retrieve the data. The API ignores the seconds portion
+                of the specified dateTime value (for example, 12:30:15 is
                 interpreted as 12:30:00 UTC).
 
-            start_date_time (datetime): Ending date/time (Coordinated Universal 
-                Time (UTC) time zone—not local— timezone) of the timespan for 
-                which to retrieve the data. The API ignores the seconds portion 
-                of the specified dateTime value (for example, 12:35:15 is 
+            start_date_time (datetime): Ending date/time (Coordinated Universal
+                Time (UTC) time zone—not local— timezone) of the timespan for
+                which to retrieve the data. The API ignores the seconds portion
+                of the specified dateTime value (for example, 12:35:15 is
                 interpreted as 12:35:00 UTC).
 
             access_token (str): This is the access_token value received from the
@@ -1415,10 +1450,10 @@ class Standard:
                                 login response
 
         Returns:
-            dict: Returns a dictionary containing the record Ids that were 
+            dict: Returns a dictionary containing the record Ids that were
                 updated and the latest record updated in that window.
                 {
-                    'ids': ['<id 1>', ..., '<id N>'], 
+                    'ids': ['<id 1>', ..., '<id N>'],
                     'latestDateCovered': '2020-03-13T12:00:00.000+0000'
                 }
         """
@@ -1506,37 +1541,37 @@ class Standard:
     @staticmethod
     def get_deleted(object_name, start_date_time, end_date_time, access_token, instance_url):
         """
-        Retrieves the list of individual records that have been deleted within 
-        the given timespan for the specified object. SObject Get Deleted is 
+        Retrieves the list of individual records that have been deleted within
+        the given timespan for the specified object. SObject Get Deleted is
         available in API version 29.0 and later.
 
-        This resource is commonly used in data replication applications. Note 
+        This resource is commonly used in data replication applications. Note
         the following considerations:
-            * Results are returned for no more than 30 days previous to the day 
+            * Results are returned for no more than 30 days previous to the day
               the call is executed.
-            * Your client application can replicate any objects to which it has 
-              sufficient permissions. For example, to replicate all data for 
-              your organization, your client application must be logged in with 
-              “View All Data” access rights to the specified object. Similarly, 
+            * Your client application can replicate any objects to which it has
+              sufficient permissions. For example, to replicate all data for
+              your organization, your client application must be logged in with
+              “View All Data” access rights to the specified object. Similarly,
               the objects must be within your sharing rules.
-            * There is a limit of 600,000 IDs returned from this resource. If 
-              more than 600,000 IDs would be returned, EXCEEDED_ID_LIMIT is 
-              returned. You can correct the error by choosing start and end 
+            * There is a limit of 600,000 IDs returned from this resource. If
+              more than 600,000 IDs would be returned, EXCEEDED_ID_LIMIT is
+              returned. You can correct the error by choosing start and end
               dates that are closer together.
 
         Args:
             object_name (str): The name of the object to get deleted records for.
 
-            startDateTime (datetime): Starting date/time (Coordinated Universal 
-                Time (UTC) time zone—not local— timezone) of the timespan for 
-                which to retrieve the data. The API ignores the seconds portion 
-                of the specified dateTime value (for example, 12:30:15 is 
+            startDateTime (datetime): Starting date/time (Coordinated Universal
+                Time (UTC) time zone—not local— timezone) of the timespan for
+                which to retrieve the data. The API ignores the seconds portion
+                of the specified dateTime value (for example, 12:30:15 is
                 interpreted as 12:30:00 UTC).
 
-            start_date_time (datetime): Ending date/time (Coordinated Universal 
-                Time (UTC) time zone—not local— timezone) of the timespan for 
-                which to retrieve the data. The API ignores the seconds portion 
-                of the specified dateTime value (for example, 12:35:15 is 
+            start_date_time (datetime): Ending date/time (Coordinated Universal
+                Time (UTC) time zone—not local— timezone) of the timespan for
+                which to retrieve the data. The API ignores the seconds portion
+                of the specified dateTime value (for example, 12:35:15 is
                 interpreted as 12:35:00 UTC).
 
             access_token (str): This is the access_token value received from the
@@ -1546,10 +1581,10 @@ class Standard:
                                 login response
 
         Returns:
-            dict: Returns a dictionary containing the record Ids that were 
+            dict: Returns a dictionary containing the record Ids that were
                 deleted and the latest record deleted in that window.
                 {
-                    'ids': ['<id 1>', ..., '<id N>'], 
+                    'ids': ['<id 1>', ..., '<id N>'],
                     'latestDateCovered': '2020-03-13T12:00:00.000+0000'
                 }
         """
@@ -1560,7 +1595,7 @@ class Standard:
         header_details = Util.get_standard_header(access_token)
 
         response = webservice.Tools.get_http_response(
-            instance_url + Standard.base_standard_uri + 'v' + API_VERSION + get_deleted_uri, 
+            instance_url + Standard.base_standard_uri + 'v' + API_VERSION + get_deleted_uri,
             header_details)
         json_response = json.loads(response.text)
 
@@ -1679,7 +1714,7 @@ class Standard:
             instance_url (str): This is the instance_url value received from the
                 login response
         Returns:
-            dict: Returns a dictionary with the metadata description of the 
+            dict: Returns a dictionary with the metadata description of the
                 given object.
         """
         describe_uri = "/sobjects/{}/describe/".format(object_name)
